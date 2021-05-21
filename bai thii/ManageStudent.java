37f3d8327c3367a1de913e82c8dd7ad7023c822b
duclong29 <73330654+duclong29@users.noreply.github.com>
2021-05-21 15:47:53 +0700

    nop muon
    nop muon

---
+++ bai thii/ManageStudent.java
@@ -0,0 +1,82 @@
+package baithi;
+
+import baithi.Student;
+
+import java.sql.*;
+import java.util.AbstractList;
+import java.util.ArrayList;
+
+public class ManageStudent {
+    private String myStudent;
+    private AbstractList<Student> studentList;
+
+    public ManageStudent (String myStudent) {
+        this.myStudent = myStudent;
+        this.studentList = new ArrayList<Student>();
+    }
+
+    public boolean addNewStudent(Student student){
+        if(findStudent(student.getStudentID())>=0){
+            System.out.println("Student is already on file");
+        }
+        studentList.add(student);
+        return true;
+    }
+
+    private  int findStudent(Student student){
+        return this.studentList.indexOf(student);
+    }
+
+    private int findStudent(String studentID){
+        for(int i =0; i < studentList.size(); i++) {
+            // Creating another temporary object to hold the name and compare
+            Student student = this.studentList.get(i);
+            if(student.getStudentID().equals(studentID)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void printStudentList() {
+        System.out.println("StudentID     Student Name       Address                Phone");
+        for(int i = 0; i < this.studentList.size(); i++) {
+            System.out.println(
+                    this.studentList.get(i).getStudentID() +"     "+
+                            this.studentList.get(i).getStudentName()+ "       "+
+                            this.studentList.get(i).getAddress() +"       "+
+                            this.studentList.get(i).getPhone());
+        }
+    }
+
+    public void save(){
+        try(
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/student",
+                        "root", "");
+                Statement stmt = conn.createStatement();
+        ) {
+            String insert1= "insert into student values(?,?,?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(insert1);
+            int rowsInserted = 0;
+            for(int i = 0; i < this.studentList.size(); i++){
+                pstmt.setString(1,this.studentList.get(i).getStudentID());
+                pstmt.setString(2,this.studentList.get(i).getStudentName());
+                pstmt.setString(3,this.studentList.get(i).getAddress());
+                pstmt.setString(4, this.studentList.get(i).getPhone());
+                rowsInserted = pstmt.executeUpdate();
+            }
+            System.out.println(rowsInserted + " student inserted!");
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public Student queryStudent(String studentID) {
+        int position = findStudent(studentID);
+        if(position >=0) {
+            return this.studentList.get(position);
+        }
+        return null;
+    }
+}
